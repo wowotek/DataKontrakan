162debdca119ec5f18ae731828371bec737727ac
Unknown <erlanggaibr2@gmail.com>
2018-04-15 05:31:12 +0700

    ~WIP 3 -> Redo the Indentation

--- src/com/wowotek/dk/classinstance/Pemasukan.java
+++ src/com/wowotek/dk/classinstance/Pemasukan.java
@@ -3,36 +3,36 @@ package com.wowotek.dk.classinstance;
 public class Pemasukan
 {
 
-  public int ID;
-  public String TRANSAKSI;
-  public String SUMBER;
-  public int JUMLAH;
+    public int ID;
+    public String TRANSAKSI;
+    public String SUMBER;
+    public int JUMLAH;
 
-  public Pemasukan(int id, String transaksi, String sumber, int jumlah)
-  {
-    this.ID = id;
-    this.TRANSAKSI = transaksi;
-    this.SUMBER = sumber;
-    this.JUMLAH = jumlah;
-  }
+    public Pemasukan(int id, String transaksi, String sumber, int jumlah)
+    {
+        this.ID = id;
+        this.TRANSAKSI = transaksi;
+        this.SUMBER = sumber;
+        this.JUMLAH = jumlah;
+    }
 
-  public void setID(int ID)
-  {
-    this.ID = ID;
-  }
+    public void setID(int ID)
+    {
+        this.ID = ID;
+    }
 
-  public void setTransaksi(String TRANSAKSI)
-  {
-    this.TRANSAKSI = TRANSAKSI;
-  }
+    public void setTransaksi(String TRANSAKSI)
+    {
+        this.TRANSAKSI = TRANSAKSI;
+    }
 
-  public void setSumber(String SUMBER)
-  {
-    this.SUMBER = SUMBER;
-  }
+    public void setSumber(String SUMBER)
+    {
+        this.SUMBER = SUMBER;
+    }
 
-  public void setJumlah(int JUMLAH)
-  {
-    this.JUMLAH = JUMLAH;
-  }
+    public void setJumlah(int JUMLAH)
+    {
+        this.JUMLAH = JUMLAH;
+    }
 }
