ea23184395558b50ebf1a3aa365a01d5239c5604
Wowotek <erlanggaibr2@gmail.com>
2018-04-16 11:50:42 +0700

    Finalizing Python Auth Process

--- src/com/wowotek/dk/auth/__proc__.py
+++ src/com/wowotek/dk/auth/__proc__.py
@@ -9,16 +9,15 @@ def std_hash(data):
 
 def create_block(data1, data2):
     
-    hd1 = data1
+    hd1 = encode64(data1)
     for _ in range(2):
         hd1 = std_hash(hd1)
 
-    hd2 = data2
+    hd2 = encode64(data2)
     for _ in range(2):
         hd2 = std_hash(hd2)
 
     new_data = std_hash(hd1) + hd2 + hd1 + std_hash(hd2)
             
-    return new_data
-
-print(create_block("wowotek", "tekoajaib"))
+    return std_hash(encode64(new_data))
+    
---
+++ src/com/wowotek/dk/auth/login_auth.py
@@ -0,0 +1,4 @@
+from __proc__ import *
+
+username = input("username : ")
+password = input("password : ")
