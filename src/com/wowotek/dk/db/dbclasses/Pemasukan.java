6626efce28ca25f1e1ef358abdf81fd6f4193f28
Wowotek <erlanggaibr2@gmail.com>
2018-04-18 17:08:09 +0700

    Finalizing Class Instance

--- src/com/wowotek/dk/db/dbclasses/Pemasukan.java
+++ src/com/wowotek/dk/db/dbclasses/Pemasukan.java
@@ -2,11 +2,10 @@ package com.wowotek.dk.db.dbclasses;
 
 public class Pemasukan
 {
-
-    public int ID;
-    public String TRANSAKSI;
-    public String SUMBER;
-    public int JUMLAH;
+    public final int ID;
+    public final String TRANSAKSI;
+    public final String SUMBER;
+    public final int JUMLAH;
 
     public Pemasukan(int id, String transaksi, String sumber, int jumlah)
     {
@@ -14,25 +13,5 @@ public class Pemasukan
         this.TRANSAKSI = transaksi;
         this.SUMBER = sumber;
         this.JUMLAH = jumlah;
-    }
-
-    public void setID(int ID)
-    {
-        this.ID = ID;
-    }
-
-    public void setTransaksi(String TRANSAKSI)
-    {
-        this.TRANSAKSI = TRANSAKSI;
-    }
-
-    public void setSumber(String SUMBER)
-    {
-        this.SUMBER = SUMBER;
-    }
-
-    public void setJumlah(int JUMLAH)
-    {
-        this.JUMLAH = JUMLAH;
     }
 }
--- src/com/wowotek/dk/db/dbclasses/Pengeluaran.java
+++ src/com/wowotek/dk/db/dbclasses/Pengeluaran.java
@@ -1,31 +1,15 @@
 package com.wowotek.dk.db.dbclasses;
 
-public class Pengeluaran
+public final class Pengeluaran
 {
-
-    public int ID;
-    public String TRANSAKSI;
-    public int JUMLAH;
+    public final int ID;
+    public final String TRANSAKSI;
+    public final int JUMLAH;
 
     public Pengeluaran(int newID, String newTransaksi, int newJumlah)
     {
         this.ID = newID;
         this.TRANSAKSI = newTransaksi;
-        this.JUMLAH = newJumlah;
-    }
-
-    public void setID(int newID)
-    {
-        this.ID = newID;
-    }
-
-    public void setTransaksi(String newTransaksi)
-    {
-        this.TRANSAKSI = newTransaksi;
-    }
-
-    public void setJumlah(int newJumlah)
-    {
         this.JUMLAH = newJumlah;
     }
 }
