b5d2822e55ec7df64c549674f95c0b138ab2722e
Wowotek <erlanggaibr2@gmail.com>
2018-04-20 00:13:20 +0700

    Added Verbose Function

--- src/com/wowotek/dk/Main.java
+++ src/com/wowotek/dk/Main.java
@@ -1,41 +1,26 @@
 package com.wowotek.dk;
 
+import com.wowotek.dk.db.DBConnection;
+import com.wowotek.dk.db.DBPemasukan;
+
 
 public class Main
 {
+    private static ErrorReporting er;
+    private static DBConnection dbc;
 
     public static void main(String[] args)
     {
+        er = new ErrorReporting(true);
+        dbc = new DBConnection(er);
+
+        closingCeremonies();
     }
 
-//    private static ErrorReporting er;
-//    private static DBConnection dbc;
-//
-//    public static void main(String[] args)
-//    {
-//        er = new ErrorReporting();
-//        dbc = new DBConnection(er);
-//
-//        if (dbc.DATABASE_CONNECTION_STATUS == false)
-//        {
-//            closingCeremonies();
-//        }
-//        else
-//        {
-//            for (int i = 0; i < 10; i++)
-//            {
-//                DBPemasukan dbp = new DBPemasukan(dbc.c, er);
-//                dbp.ambilDataPemasukan();
-//            }
-//        }
-//
-//        closingCeremonies();
-//    }
-//
-//    private static void closingCeremonies()
-//    {
-//        er.debug("Closing Program...", 4);
-//        dbc.closeConnection();
-//        er.closeFile();
-//    }
+    private static void closingCeremonies()
+    {
+        er.debug("Closing Program...", 4);
+        dbc.closeConnection();
+        er.closeFile();
+    }
 }
